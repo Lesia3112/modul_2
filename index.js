c80740b31cc00e7a2c4ef92232e3264b0104617c
Lesia3112 <lmachiha31@gmail.com>
2022-06-06 19:16:47 +0300

    +

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const headerBlock = document.getElementById('js-header-block'),
-burger = document.getElementById('js-header-burger');
+// const headerBlock = document.getElementById('js-header-block');
+// const burger = document.getElementById('js-header-burger');
+const modal = () =>{
 
-burger.addEventListener('click',headerBlock)=() => {
-  headerBlock.addClassList.toggle('show');
-}
-console.log(headerBlock);
+  document.getElementById('js-header-block').classList.toggle('show');
+  // return (modal);
+};
+document.getElementById('js-header-burger').addEventListener('click', modal);
+
+// let modalNavigation=burger.addEventListener('click'=() => {
+//   headerBlock.classList.toggle('show')
+// });
+// console.log(modal);
