f5ceddb64eda9c08b2f4ea55cfa3c3d8586df710
Lesia3112 <lmachiha31@gmail.com>
2022-07-12 12:20:59 +0300

    +comments;

--- index.js
+++ index.js
@@ -1,33 +1,91 @@
-// modal window
+// !!!зміна беграунду хедера при скролі
+
+
+const header = document.getElementById('js-header')
+
+const scrollListener = () => {
+  window.scrollY > 10
+    ? header.classList.add('header__scroll')
+    : header.classList.remove('header__scroll')
+}
+scrollListener();
+document.addEventListener('scroll', scrollListener)
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     document.getElementById("js-header").className = "header__scroll";
+//   } else {
+//     document.getElementById("js-header").className = "";
+//   }
+// }
+// console.log(myFunction());
+
+//! modal window
 
 const modal = () => {
-  document.getElementById("js-header-block").classList.toggle("show");
-};
-document.getElementById("js-header-burger")?.addEventListener("click", modal);
+  document.getElementById('js-header-block').classList.toggle('show')
+}
+document.getElementById('js-header-burger')?.addEventListener('click', modal)
 
 // !Swiper
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
   // Optional parameters
-  slidesPerView: 3,
+  slidesPerView: 1,
+  centeredSlidesBounds: true,
   spaceBetween: 50,
   loop: true,
-  // effect: 'flip',
-  // EffectFlip: {
-  //   slideShadows: false,
-  // },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   // If we need pagination
 
   pagination: {
-    el: ".swiper-pagination",
-    clicable: true,
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
   },
-});
+
+  breakpoints: {
+    420: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    // when window width is >= 480px
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+})
+
+// автоматична прокрутка слайдера
+const swiper1 = document.querySelector('.swiper').swiper
+// setInterval( () => {
+//   swiper1.slideNext();
+// },2000);
+
+// добавляння кастомних кнопок
+document.getElementById('js-slider-button-prev'),
+  addEventListener('click', () => {
+    swiper1.slidePrev()
+  })
+document.getElementById('js-slider-button-next'),
+  addEventListener('click', () => {
+    swiper1.slideNext()
+  })
+
+// зміна пагінації
+
 
 // google-maps
 // function initMap() {
@@ -43,200 +101,64 @@ const swiper = new Swiper(".swiper", {
 
 //   })
 // };
-console.log("9999999999");
-window.initMap = function () {
-  const location = { lat: 40.622112332807006, lng: -74.03019098518402 };
-  const el = document.getElementById("js-map");
-  console.log('el',el);
-  const map = new google.maps.Map(el, {
-    zoom: 15,
-    center: location,
-    styles: [
-      {
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#F5F5F5",
-          },
-        ],
-      },
-      {
-        elementType: "labels.icon",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#616161",
-          },
-        ],
-      },
-      {
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#F5F5F5",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#BDBDBD",
-          },
-        ],
-      },
-      {
-        featureType: "poi",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#EEEEEE",
-          },
-        ],
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#757575",
-          },
-        ],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#E5E5E5",
-          },
-        ],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#9E9E9E",
-          },
-        ],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#FFFFFF",
-          },
-        ],
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#757575",
-          },
-        ],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#DADADA",
-          },
-        ],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#616161",
-          },
-        ],
-      },
-      {
-        featureType: "road.local",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#9E9E9E",
-          },
-        ],
-      },
-      {
-        featureType: "transit.line",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#E5E5E5",
-          },
-        ],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#EEEEEE",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#C9C9C9",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#9E9E9E",
-          },
-        ],
-      },
-    ],
-  });
-};
 
 // form registration
 const toggleError = (id, error) => {
-  const el = document.getElementById(id);
-  error ? el.classList.add("error") : el.classList.remove("error");
-};
-// form registration
-// Get Dom element
+  const el = document.getElementById(id)
+  error ? el.classList.add('error') : el.classList.remove('error')
+}
+// form registration Get Dom element
 const registration = (event) => {
   // відміна стандартної поведінки форми
-  event.preventDefault();
+  event.preventDefault()
 
-  const form = new FormData(event.target);
+  const form = new FormData(event.target)
 
-  const userName = form.get("userName");
-  toggleError("js-form-registration-user-name", !userName);
+  const userName = form.get('userName')
+  toggleError('js-form-registration-user-name', !userName)
 
-  const userEmail = form.get("userEmail");
-  toggleError("js-form-registration-user-email", !userEmail);
+  const userEmail = form.get('userEmail')
+  toggleError('js-form-registration-user-email', !userEmail)
 
-  console.log(userName, userEmail);
-};
+  console.log(userName, userEmail)
+}
 document
-  .getElementById("js-form-registration")?.addEventListener("submit", registration);
-document.addEventListener("submit", (event) => {
-  event.target.reset();
-});
+  .getElementById('js-form-registration')
+  ?.addEventListener('submit', registration)
+document.addEventListener('submit', (event) => {
+  event.target.reset()
+})
+
+//! валідація форми емейл ВАЖЛИВО
+// const email = document.getElementById('js-form-registration-user-email')
+
+// email.addEventListener('input', function (event) {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity('D')
+//   } else {
+//     email.setCustomValidity('')
+//   }
+// })
+
+// Fetch
+
+const URL = 'https://jsonplaceholder.typicode.com/posts/1'
+// const submitButton = document.getElementById('js-submit-button');
+
+// submitButton.addEventListener('click', () => {
+//   console.log(000);
+//   fetch('URL')
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log('DATA', data);
+//     });
+// });
+const test = document.getElementById('js-button-test')
+test.addEventListener('click', () => {
+  console.log(111)
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('DATA', data)
+    })
+})
---
+++ map.js
@@ -0,0 +1,169 @@
+// console.log("9999999999");
+// window.initMap = function () {
+//   const location = { lat: 40.71314644913046, lng: -74.01316353558238 };
+//   const myPin = './../assets/icons/pin.png';
+//   const map = new google.maps.Map(document.getElementById('js-map'), {
+//     zoom: 15,
+//     center: location,
+    // styles: [
+    //   {
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#F5F5F5",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     elementType: "labels.icon",
+    //     stylers: [
+    //       {
+    //         visibility: "off",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#616161",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     elementType: "labels.text.stroke",
+    //     stylers: [
+    //       {
+    //         color: "#F5F5F5",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "administrative.land_parcel",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#BDBDBD",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "poi",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#EEEEEE",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "poi",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#757575",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "poi.park",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#E5E5E5",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "poi.park",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#9E9E9E",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "road",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#FFFFFF",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "road.arterial",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#757575",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "road.highway",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#DADADA",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "road.highway",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#616161",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "road.local",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#9E9E9E",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "transit.line",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#E5E5E5",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "transit.station",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#EEEEEE",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "water",
+    //     elementType: "geometry",
+    //     stylers: [
+    //       {
+    //         color: "#C9C9C9",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     featureType: "water",
+    //     elementType: "labels.text.fill",
+    //     stylers: [
+    //       {
+    //         color: "#9E9E9E",
+    //       },
+    //     ],
+    //   },
+    // ],
+//   });
+// };
