b8c3db756e10c6c971b45759556a5dec4e7519e0
Lesia3112 <lmachiha31@gmail.com>
2022-07-13 15:44:28 +0300

    change;

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // !!!зміна беграунду хедера при скролі
-
-
 const header = document.getElementById('js-header')
 
 const scrollListener = () => {
@@ -8,22 +6,10 @@ const scrollListener = () => {
     ? header.classList.add('header__scroll')
     : header.classList.remove('header__scroll')
 }
-scrollListener();
+scrollListener()
 document.addEventListener('scroll', scrollListener)
 
-// window.onscroll = function() {myFunction()};
-
-// function myFunction() {
-//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//     document.getElementById("js-header").className = "header__scroll";
-//   } else {
-//     document.getElementById("js-header").className = "";
-//   }
-// }
-// console.log(myFunction());
-
 //! modal window
-
 const modal = () => {
   document.getElementById('js-header-block').classList.toggle('show')
 }
@@ -74,7 +60,7 @@ const swiper1 = document.querySelector('.swiper').swiper
 //   swiper1.slideNext();
 // },2000);
 
-// добавляння кастомних кнопок
+// добавлення кастомних кнопок
 document.getElementById('js-slider-button-prev'),
   addEventListener('click', () => {
     swiper1.slidePrev()
@@ -83,24 +69,6 @@ document.getElementById('js-slider-button-next'),
   addEventListener('click', () => {
     swiper1.slideNext()
   })
-
-// зміна пагінації
-
-
-// google-maps
-// function initMap() {
-//   const baseLocation = { lat: 40.678234565672625, lng: -73.94328990937252 };
-
-//   const map = new google.maps.Map(document.getElementById('js-map'),{
-//     zoon:15,
-//     center: baseLocation,
-//    });
-//   const marker = new google.maps.Marker({
-//     position:baseLocation ,
-//     map: map,
-
-//   })
-// };
 
 // form registration
 const toggleError = (id, error) => {
@@ -129,30 +97,9 @@ document.addEventListener('submit', (event) => {
   event.target.reset()
 })
 
-//! валідація форми емейл ВАЖЛИВО
-// const email = document.getElementById('js-form-registration-user-email')
-
-// email.addEventListener('input', function (event) {
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity('D')
-//   } else {
-//     email.setCustomValidity('')
-//   }
-// })
-
 // Fetch
-
 const URL = 'https://jsonplaceholder.typicode.com/posts/1'
-// const submitButton = document.getElementById('js-submit-button');
 
-// submitButton.addEventListener('click', () => {
-//   console.log(000);
-//   fetch('URL')
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('DATA', data);
-//     });
-// });
 const test = document.getElementById('js-button-test')
 test.addEventListener('click', () => {
   console.log(111)
